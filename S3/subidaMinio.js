f61ea7d199926ebed2e22a5fd06e5aeb17323f6f
daviro05 <daviro05@ucm.es>
2018-11-22 15:51:47 +0100

    Subida con barras en paralelo

--- S3/subidaMinio.js
+++ S3/subidaMinio.js
@@ -6,72 +6,67 @@ function upload() {
 
     document.querySelector('.info-subida').style.display = "block";
     document.querySelector('.cuadro-subida').style.display = "none";
+    document.querySelector('.btn-subida').disabled = true;
     
-
-    console.log("Ámbito: " + ambito + " - Ficheros: " + ficheros.length);
-    $('#status').text(`Preparando ficheros...`);
-
-    var tam_total = 0;
-    var procesado = false;
+    $('#status').text(`Ficheros a subir`);
 
     Array.from(ficheros).forEach(function (elemento, index, array) {
-        var file = elemento;
-        tam_total += file.size;
+        var arrayFich = [];
 
-        if(index === array.length-1){
-           console.log("Tamaño total de ficheros: "+tam_total);
-            procesado = true;
-        }
+        arrayFich.push(elemento,index);
+
+        document.querySelector(".barras").innerHTML += `<div class="progress">
+                    <div id="barra${index}" class="progress-bar progress-bar-striped" style="width:0%"></div>
+                    </div>
+                    <p id="infobarra${index}"></p>
+                    <p id="time${index}"><p>`;
 
         // Retrieve a URL from our server.
-        retrieveNewURL(file,ambito,url => {
+        retrieveNewURL(arrayFich,ambito,url => {
           // Upload the file to the server.
-          console.log("Pasa por aqui");
-          uploadFile(file,procesado,url);
+          uploadFile(arrayFich,url);
         })
     });
 }
 
  // Request to our Node.js server for an upload URL.
- function retrieveNewURL(file,ambito,cb) {
-    $.get(`/subirFichero?name=${file.name}&ambito=${ambito}`, (url) => {
+ function retrieveNewURL(arrayFich,ambito,cb) {
+    $.get(`/subirFichero?name=${arrayFich[0].name}&ambito=${ambito}`, (url) => {
       cb(url)
     });
   }
  
   // Use XMLHttpRequest to upload the file to S3.
-  function uploadFile(file, procesado,url) {
+  function uploadFile(arrayFich, url) {
     var xhr = new XMLHttpRequest ()
     xhr.open('PUT', url, true);
-    progreso(xhr,procesado,file);
-    xhr.send(file);
+    progreso(xhr,arrayFich);
+    xhr.send(arrayFich[0]);
   }
 
-  function progreso(xhr,procesado,file){
+  function progreso(xhr,arrayFich){
 
     var started_at = new Date();
     var seconds_elapsed, bytes_per_second, remaining_bytes, seconds;
 
     xhr.upload.onprogress = function (e) {
+            document.querySelector("#infobarra"+arrayFich[1]).innerText = `Subiendo ${arrayFich[0].name}...`;
 
-        $('#status').text(`Subiendo ${file.name}...`);
-        document.querySelector('.btn-subida').disabled = true;
-
-        if(procesado){
-
-            document.querySelector(".progress-bar").innerText = Math.floor((e.loaded / e.total) * 100) + '%';
-            document.querySelector(".progress-bar").style.width = Math.floor((e.loaded / e.total) * 100) + '%';
-
-            seconds_elapsed =   ( new Date().getTime() - started_at.getTime() )/1000; 
-            bytes_per_second =  seconds_elapsed ? e.loaded / seconds_elapsed : 0 ;
-            remaining_bytes =   e.total - e.loaded;
-            seconds = seconds_elapsed ? remaining_bytes / bytes_per_second : 'Calculando...' ;
-            $('.time_re').text("Tiempo restante aproximado: "+(seconds/60).toFixed(0) + " minutos");
-        }
+            if(e.lengthComputable){
+                document.querySelector("#barra"+arrayFich[1]).innerText = Math.floor((e.loaded / e.total) * 100) + '%';
+                document.querySelector("#barra"+arrayFich[1]).style.width = Math.floor((e.loaded / e.total) * 100) + '%';
+        
+                seconds_elapsed =   ( new Date().getTime() - started_at.getTime() )/1000; 
+                bytes_per_second =  seconds_elapsed ? e.loaded / seconds_elapsed : 0 ;
+                remaining_bytes =   e.total - e.loaded;
+                seconds = seconds_elapsed ? remaining_bytes / bytes_per_second : 'Calculando...' ;
+                document.querySelector("#time"+arrayFich[1]).innerText = "Restante: "+(seconds/60).toFixed(0)+ " minutos";
+            }
     }
 
     xhr.upload.onloadend = function (e) {
-        console.log(file.name + " subido con éxito");
-        $('#status').text(`Subida completada`);
+        console.log(arrayFich[0].name + " subido con éxito");
+        document.querySelector("#infobarra"+arrayFich[1]).innerText = `${arrayFich[0].name} subido`;
+        document.querySelector("#time"+arrayFich[1]).innerText = "";
     }
   }
