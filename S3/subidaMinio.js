0a6a19534f0e0910ae2865d0ba88cf6d9caf7db6
daviro05 <daviro05@ucm.es>
2018-11-22 13:13:47 +0100

    Limpieza de código

--- S3/subidaMinio.js
+++ S3/subidaMinio.js
@@ -13,59 +13,54 @@ function upload() {
 
     var tam_total = 0;
     var procesado = false;
-    var aux = 0;
 
     Array.from(ficheros).forEach(function (elemento, index, array) {
         var file = elemento;
-        
         tam_total += file.size;
-        if(aux < file.size)
-            aux = file.size
 
         if(index === array.length-1){
-            console.log(tam_total);
+           console.log("Tamaño total de ficheros: "+tam_total);
             procesado = true;
         }
 
         // Retrieve a URL from our server.
         retrieveNewURL(file,ambito,url => {
           // Upload the file to the server.
-          uploadFile(file, procesado, tam_total, aux, url);
+          console.log("Pasa por aqui");
+          uploadFile(file,procesado,url);
         })
     });
 }
 
  // Request to our Node.js server for an upload URL.
- function retrieveNewURL(file,ambito, cb) {
+ function retrieveNewURL(file,ambito,cb) {
     $.get(`/subirFichero?name=${file.name}&ambito=${ambito}`, (url) => {
       cb(url)
     });
   }
  
   // Use XMLHttpRequest to upload the file to S3.
-  function uploadFile(file, procesado, tam_total,url) {
+  function uploadFile(file, procesado,url) {
     var xhr = new XMLHttpRequest ()
     xhr.open('PUT', url, true);
-    progreso(xhr,procesado, tam_total,file);
+    progreso(xhr,procesado,file);
     xhr.send(file);
   }
 
-  function progreso(xhr,procesado, tam_total,file){
+  function progreso(xhr,procesado,file){
 
     var started_at = new Date();
     var seconds_elapsed, bytes_per_second, remaining_bytes, seconds;
 
     xhr.upload.onprogress = function (e) {
+
         $('#status').text(`Subiendo ${file.name}...`);
         document.querySelector('.btn-subida').disabled = true;
 
         if(procesado){
-            if (e.lengthComputable) {
-                console.log(e.loaded + "/" + e.total);
-                //console.log(Math.floor((e.loaded / e.total) * 100) + '%');
-                document.querySelector(".progress-bar").innerText = Math.floor((e.loaded /e.total) * 100) + '%';
-                document.querySelector(".progress-bar").style.width = Math.floor((e.loaded / e.total) * 100) + '%';
-            }
+
+            document.querySelector(".progress-bar").innerText = Math.floor((e.loaded / e.total) * 100) + '%';
+            document.querySelector(".progress-bar").style.width = Math.floor((e.loaded / e.total) * 100) + '%';
 
             seconds_elapsed =   ( new Date().getTime() - started_at.getTime() )/1000; 
             bytes_per_second =  seconds_elapsed ? e.loaded / seconds_elapsed : 0 ;
